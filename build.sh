bb4d1983597f3350750fe55d7b2bbae9b46cf380
TxThinking <cloud@txthinking.com>
2022-01-26 16:08:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,6 @@ deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/jinbe_linux
 deno compile -A -r --unstable --target x86_64-apple-darwin -o _/jinbe_darwin_amd64 https://raw.githubusercontent.com/txthinking/jinbe/master/main.js
 deno compile -A -r --unstable --target aarch64-apple-darwin -o _/jinbe_darwin_arm64 https://raw.githubusercontent.com/txthinking/jinbe/master/main.js
 
-#install upx first
-upx _/*
-
 nami release github.com/txthinking/jinbe $1 _
 
 rm -rf _
