0c4b5ed141340f4c858ffb3df424a2ac5dfd5dc4
TxThinking <cloud@txthinking.com>
2022-01-26 15:59:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ mkdir _
 
 deno compile -A -r --unstable --target x86_64-unknown-linux-gnu -o _/jinbe_linux_amd64 https://raw.githubusercontent.com/txthinking/jinbe/master/main.js
 deno compile -A -r --unstable --target x86_64-apple-darwin -o _/jinbe_darwin_amd64 https://raw.githubusercontent.com/txthinking/jinbe/master/main.js
+deno compile -A -r --unstable --target aarch64-apple-darwin -o _/jinbe_darwin_arm64 https://raw.githubusercontent.com/txthinking/jinbe/master/main.js
 
 #install upx first
 upx _/*
